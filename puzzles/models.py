bfce0fc9db8b9aa7f9210afa603106d309c2755d
Reid Barton <rwbarton@gmail.com>
2012-01-09 05:27:27 +0000

    Register users on account creation.

--- puzzles/models.py
+++ puzzles/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+import re
+
+from puzzles.ejabberd import ejabberdctl
+
 class Status(models.Model):
     text = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
@@ -68,3 +74,46 @@ class TagTagListRelation(models.Model):
 
 class Motd(models.Model):
     motd = models.TextField(blank=True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    has_jabber_account = models.BooleanField()
+
+def jabber_username(user):
+    first = ''.join(re.findall("\w", user.first_name))
+    last = ''.join(re.findall("\w", user.last_name))
+    return first + '.' + last[0:1]
+
+def jabber_host():
+    return 'metaphysical.no-ip.org'
+
+_jabber_password = None
+def jabber_password():
+    global _jabber_password
+    if _jabber_password is None:
+        _jabber_password = open('/etc/metaphysical/jabber-password').read()
+    return _jabber_password
+
+def create_jabber_user(**kwargs):
+    try:
+        user = kwargs['instance']
+        try:
+            user_profile = UserProfile.objects.get(user=user)
+        except UserProfile.DoesNotExist:
+            user_profile = UserProfile.objects.create(user=user, has_jabber_account=False)
+        if not user_profile.has_jabber_account and user.first_name != '':
+            username = jabber_username(user)
+            with ejabberdctl() as e:
+                e('register', username, jabber_host(), jabber_password())
+                e('set_vcard', username, jabber_host(), 'FN', user.first_name + ' ' + user.last_name)
+                e('set_vcard2', username, jabber_host(), 'N', 'FAMILY', user.last_name)
+                e('set_vcard2', username, jabber_host(), 'N', 'GIVEN', user.first_name)
+                e('push_alltoall', jabber_host(), 'Puzzlers')
+            user_profile.has_jabber_account = True
+            user_profile.save()
+    except Exception, e:
+        import traceback
+        traceback.print_exc()
+
+post_save.connect(create_jabber_user, sender=User)
