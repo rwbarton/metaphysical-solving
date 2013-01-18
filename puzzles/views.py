80283b9a947709aa398afb8ca63e8cb5b721f3bc
Reid Barton <rwbarton@gmail.com>
2013-01-18 13:03:32 -0500

    Only show "sign into Humbug" if they have an account already

--- puzzles/views.py
+++ puzzles/views.py
@@ -28,6 +28,7 @@ def puzzle_context(request, d):
     d1['path'] = request.path
     if 'body' in request.GET:
         d1['body_only'] = True
+    d1['has_humbug'] = request.user.userprofile.finished_humbug_registration()
     d1['humbug_email'] = user_to_email(request.user)
     return RequestContext(request, d1)
 
