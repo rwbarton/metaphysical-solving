a87c82771e287eb98452065a5ea68f1e635bc2d2
Yi-Hsin Lin <ylin@kyruus.com>
2015-12-07 20:36:10 -0500

    TEMPLATE_DIRS should be a tuple

--- solving/settings.py
+++ solving/settings.py
@@ -109,7 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'templates'
+    'templates',
 )
 
 LOGIN_URL = '/login/google-oauth2/'
