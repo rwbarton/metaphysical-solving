fae0fcaee1a8a4b0a19fd6300553fb7c278754da
Yi-Hsin Lin <ylin@kyruus.com>
2015-12-17 01:06:32 -0500

    Use oauth2 to authenticate to Google Spreadsheets

--- puzzles/googlespreadsheet.py
+++ puzzles/googlespreadsheet.py
@@ -2,6 +2,8 @@ import gdata.docs.data
 import gdata.docs.client
 import gdata.acl.data
 import json
+from oauth2client.client import SignedJwtAssertionCredentials
+
 
 _google_config = None
 
@@ -11,10 +13,14 @@ def get_google_config():
         _google_config = json.loads(open('/etc/metaphysical/google.json').read())
     return _google_config
 
+
 def create_google_spreadsheet(title):
     google_config = get_google_config()
-    client = gdata.docs.client.DocsClient(source=google_config['source'])
-    client.ClientLogin(google_config['username'], google_config['password'], client.source)
+    scope = ['https://spreadsheets.google.com/feeds', 'https://docs.google.com/feeds']
+    credentials = SignedJwtAssertionCredentials(google_config['client_email'], google_config['private_key'].encode(), scope)
+    client = gdata.docs.client.DocsClient(source=google_config['project_id'])
+    token = gdata.gauth.OAuth2TokenFromCredentials(credentials)
+    token.authorize(client)
     spreadsheet = gdata.docs.data.Resource(gdata.docs.data.SPREADSHEET_LABEL, title)
     new_spreadsheet = client.CreateResource(spreadsheet)
     acl_entry = gdata.docs.data.AclEntry(
