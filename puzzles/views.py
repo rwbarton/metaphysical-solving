5a326b133fe4c1270b2ae0284edb4a0f5b9f013e
Reid Barton <rwbarton@gmail.com>
2016-01-13 19:35:15 -0500

    Only provide answer callin button if the puzzle has a check answer link

--- puzzles/views.py
+++ puzzles/views.py
@@ -91,7 +91,7 @@ def puzzle_info(request, puzzle_id):
                 'queued_answers': queued_answers,
                 'wrong_answers': wrong_answers,
                 'uploaded_files': uploaded_files,
-                'answer_callin': settings.ANSWER_CALLIN_ENABLED,
+                'answer_callin': settings.ANSWER_CALLIN_ENABLED and puzzle.checkAnswerLink,
                 'refresh': 30
                 }))
 
