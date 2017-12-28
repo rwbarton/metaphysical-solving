a096c716b1e260ed1cc61e2c26991a495497e6d2
Reid Barton <rwbarton@gmail.com>
2017-12-28 02:45:39 +0000

    Remove 2017 answer call-in code

--- puzzles/submit.py
+++ puzzles/submit.py
@@ -5,28 +5,4 @@ import mechanize
 import re
 
 def submit_answer(submission, is_request):
-    puzzle = submission.puzzle
-    if puzzle.checkAnswerLink:
-        check_answer = puzzle.checkAnswerLink
-    else:
-        check_answer = re.sub('https://monsters-et-manus.com/',
-                              'https://monsters-et-manus.com/submit/',
-                              puzzle.url)
-
-    br = puzzlelogin.get_logged_in_browser()
-    br.open(check_answer)
-
-    if is_request:
-        br.select_form(nr=1)
-        br['interactionrequest'] = submission.answer
-    else:
-        br.select_form(nr=0)
-        br['submission'] = submission.answer
-
-    resp = br.submit()
-    submission.response = resp.read()
-    submission.save()
-
-    if resp.code != 200:
-        raise ValueError("answer submission got %d" % (resp.code,))
-            
+    return
