24b2fa0def16b45513a3c2ef895ef291d20b2d87
Reid Barton <rwbarton@gmail.com>
2017-12-31 00:53:34 +0000

    "Submit" puzzle answers for GPH

--- puzzles/submit.py
+++ puzzles/submit.py
@@ -4,5 +4,23 @@ from puzzles import puzzlelogin
 import mechanize
 import re
 
-def submit_answer(submission, is_request):
-    return
+def submit_answer(submission):
+    puzzle = submission.puzzle
+    if puzzle.checkAnswerLink:
+        check_answer = puzzle.checkAnswerLink
+    else:
+        return
+
+    br = puzzlelogin.get_logged_in_browser()
+    br.open(check_answer)
+
+    br.select_form(nr=0)
+    br['answer'] = submission.answer
+
+    resp = br.submit()
+    submission.response = resp.read()
+    submission.save()
+
+    if resp.code != 200:
+        raise ValueError("answer submission got %d" % (resp.code,))
+
