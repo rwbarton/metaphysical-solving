c3a10cf02f506dae12496f01d9fc6f1b0f830de1
Reid Barton <rwbarton@gmail.com>
2019-01-18 20:24:41 +0000

    Revert "Puzzlescrape for GPH" + update for 2019 hunt
    This reverts commit cecab3f1c24d4e15167220681b9805b5f2dac320.

--- puzzles/management/commands/puzzlescrape.py
+++ puzzles/management/commands/puzzlescrape.py
@@ -5,7 +5,6 @@ from puzzles.models import Puzzle, Tag, AutoTag, TagList
 from lxml import etree
 import urlparse
 from datetime import datetime
-import re
 
 from puzzles import puzzlelogin
 
@@ -17,9 +16,9 @@ def create_puzzle(title, url, tag, is_meta=False):
         puzzle_page = puzzlelogin.fetch_with_single_login(url)
         doc = etree.HTML(puzzle_page)
 
-        answer_links = doc.xpath("//a[@class='btn']/span[text()='Check your answer spoiler-free']")
+        answer_links = doc.xpath("//div[@id='submit']/a[text()='Check answer']")
         if len(answer_links) == 1:
-            answer_link = answer_links[0].getparent()
+            answer_link = answer_links[0]
             checkAnswerLink = urlparse.urljoin(url, answer_link.get('href'))
         else:
             checkAnswerLink = ''
@@ -29,7 +28,7 @@ def create_puzzle(title, url, tag, is_meta=False):
             puzzle_object.tags.add(Tag.objects.get(name=tag))
             if is_meta:
                 puzzle_object.tags.add(Tag.objects.get(name='metas'))
-            print "Created puzzle (%s, %s, %s)" % (title, url, checkAnswerLink)
+            print("Created puzzle (%s, %s, %s)" % (title, url, checkAnswerLink))
         except django.db.utils.IntegrityError:
             # puzzle already exists (race)
             pass
@@ -40,45 +39,49 @@ def add_tag_to_taglist(tag, taglist_name):
     taglist.save()
 
 def html_to_tag(html):
-    return re.sub('day ', 'gph', html.lower())
+    return html
 
 class Command(BaseCommand):
     help = "Visit Hunt Overview and create new puzzles"
 
     def handle(self, *args, **kwargs):
-        overview_url = 'https://galacticpuzzlehunt.com/puzzles.html'
+        overview_url = 'https://molasses.holiday/puzzle'
 
-        print "Beginning puzzlescrape run at " + datetime.now().isoformat()
+        print("Beginning puzzlescrape run at " + datetime.now().isoformat())
 
         text = puzzlelogin.fetch_with_single_login(overview_url)
         doc = etree.HTML(text)
 
-        rnds = doc.xpath("//div[@class='three columns']/h3")
+        rnds = doc.xpath("//div[@class='round-list-header']/a")
         for rnd in rnds:
+            if rnd.text is None:
+                continue
+            title = rnd.text + ' Meta'
             rnd_name = rnd.text
+            url = rnd.get('href')
+            url = 'https://molasses.holiday' + url
 
             try:
                 auto_tag = AutoTag.objects.get(html_name=rnd_name)
                 tag = auto_tag.tag
             except AutoTag.DoesNotExist:
+                continue
                 tag, created = Tag.objects.get_or_create(name=html_to_tag(rnd_name))
                 auto_tag, _ = AutoTag.objects.get_or_create(html_name=rnd_name, tag=tag)
                 if created:
-                    add_tag_to_taglist(tag, 'gph')
+                    add_tag_to_taglist(tag, 'unsolved rounds')
+                    add_tag_to_taglist(tag, 'all rounds')
 
-            # Create per-round metapuzzle (not needed for gph?)
-
-            # title = rnd.text + ' Meta'
-            # url = rnd.get('href')
-            # url = 'https://monsters-et-manus.com' + url
+            # no round metas
             # create_puzzle(title=title, url=url, tag=tag, is_meta=True)
 
-        puzzles = doc.xpath("//table[@class='puzzles-list-table gph-list-table']/tr/td[1]/a")
+        puzzles = doc.xpath("//div[@class='puzzle-list-item']/a")
         for puzzle in puzzles:
-            title = puzzle.text.strip()
-            url = urlparse.urljoin(overview_url, puzzle.get('href'))
-            row = puzzle.getparent().getparent().getparent().getparent().getparent()
-            rnd = row.getchildren()[0].getchildren()[0].text
+            title = puzzle.text
+            url = puzzle.get('href')
+            url = 'https://molasses.holiday' + url
+            rnd = puzzle.getparent().getparent().getchildren()[0].getchildren()[0]
+            rnd = rnd.text
 
             try:
                 auto_tag = AutoTag.objects.get(html_name=rnd)
@@ -87,8 +90,9 @@ class Command(BaseCommand):
                 tag, created = Tag.objects.get_or_create(name=html_to_tag(rnd))
                 auto_tag, _ = AutoTag.objects.get_or_create(html_name=rnd, tag=tag)
                 if created:
-                    add_tag_to_taglist(tag, 'gph')
+                    add_tag_to_taglist(tag, 'unsolved rounds')
+                    add_tag_to_taglist(tag, 'all rounds')
 
             create_puzzle(title=title, url=url, tag=tag)
 
-        print "Finished puzzlescrape run"
+        print("Finished puzzlescrape run")
