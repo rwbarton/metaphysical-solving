22bb33dff63372d933fa0648b3e0effb52a80547
Reid Barton <rwbarton@gmail.com>
2021-01-15 18:24:06 +0000

    scrape wrong answers

--- puzzles/management/commands/answerscrape.py
+++ puzzles/management/commands/answerscrape.py
@@ -11,53 +11,71 @@ solved_status = Status.objects.get(text='solved!')
 class Command(BaseCommand):
     help = "Visit call-in pages and update answers in database accordingly"
 
+    def handle_wrong_answer(self, puzzle, answer):
+        print('WRONG ' + puzzle.title + ' = ' + answer)
+        PuzzleWrongAnswer.objects.get_or_create(puzzle=puzzle, answer=answer)
+
     def handle(self, *args, **kwargs):
-        print "Beginning answerscrape run at " + datetime.now().isoformat()
+        print("Beginning answerscrape run at " + datetime.now().isoformat())
 
         puzzles = Puzzle.objects.all().order_by('id')
         for puzzle in puzzles:
             if puzzle.status == solved_status:
                 continue
-            answer_url = puzzle.checkAnswerLink
-            if answer_url is None:
+
+            puzzle_prefix = 'https://perpendicular.institute/puzzle/'
+            if puzzle.url.startswith(puzzle_prefix):
+                answer_url = 'https://perpendicular.institute/embed/submit/puzzle/' + \
+                             puzzle.url[len(puzzle_prefix):]
+            else:
                 continue
 
-            text = puzzlelogin.fetch_with_single_login(answer_url)
+            print(answer_url)
+
+            text = puzzlelogin.fetch_with_single_login(answer_url).decode('utf-8')
 
             QueuedAnswer.objects.filter(puzzle=puzzle).delete()
 
-            mode = 'none'
-            skip = True
+            # skip = True
+            cur_answer = None
             for l in text.split('\n'):
-                solved_prefix = '      Solved! Answer: <b>'
-                solved_suffix = '</b><br>'
-                if l.startswith(solved_prefix) and l.endswith(solved_suffix):
-                    answer = l[len(solved_prefix):len(l)-len(solved_suffix)]
-                    print 'SOLVED ' + puzzle.title + ' = ' + answer
-                    puzzle.answer = answer
-                    puzzle.status = solved_status
-                    puzzle.save()
+                answer_prefix = '    <td class="answer">'
+                answer_suffix = '</td>'
+                if l.startswith(answer_prefix) and l.endswith(answer_suffix):
+                    cur_answer = l[len(answer_prefix):len(l)-len(answer_suffix)]
 
-                if l == '      <h3>In the Queue:</h3>':
-                    mode = 'queued'
-                if l == '      <h3>Previous Answers Submitted:</h3>':
-                    mode = 'wrong'
+                if l == '    <td>Incorrect</td>':
+                    self.handle_wrong_answer(puzzle, cur_answer)
 
-                non_answer_prefix = '\t  <td>'
-                if l.startswith(non_answer_prefix):
-                    if skip:    # Every other match is a header which we ignore
-                        skip = False
-                        continue
-                    else:
-                        skip = True
-                        rest = l[len(non_answer_prefix):]
-                        if rest.endswith('</td>'):
-                            rest = rest[:-len('</td>')]
-                        if mode == 'queued':
-                            print 'QUEUED ' + puzzle.title + ' = ' + rest
-                            QueuedAnswer.objects.get_or_create(puzzle=puzzle, answer=rest)
-                        if mode == 'wrong' and puzzle.status != solved_status:
-                            print 'WRONG ' + puzzle.title + ' = ' + rest
-                            PuzzleWrongAnswer.objects.get_or_create(puzzle=puzzle, answer=rest)
+                # solved_prefix = '      Solved! Answer: <b>'
+                # solved_suffix = '</b><br>'
+                # if l.startswith(solved_prefix) and l.endswith(solved_suffix):
+                #     answer = l[len(solved_prefix):len(l)-len(solved_suffix)]
+                #     print 'SOLVED ' + puzzle.title + ' = ' + answer
+                #     puzzle.answer = answer
+                #     puzzle.status = solved_status
+                #     puzzle.save()
 
-        print "Finished answerscrape run"
+                # if l == '      <h3>In the Queue:</h3>':
+                #     mode = 'queued'
+                # if l == '      <h3>Previous Answers Submitted:</h3>':
+                #     mode = 'wrong'
+
+                # non_answer_prefix = '\t  <td>'
+                # if l.startswith(non_answer_prefix):
+                #     if skip:    # Every other match is a header which we ignore
+                #         skip = False
+                #         continue
+                #     else:
+                #         skip = True
+                #         rest = l[len(non_answer_prefix):]
+                #         if rest.endswith('</td>'):
+                #             rest = rest[:-len('</td>')]
+                #         if mode == 'queued':
+                #             print 'QUEUED ' + puzzle.title + ' = ' + rest
+                #             QueuedAnswer.objects.get_or_create(puzzle=puzzle, answer=rest)
+                #         if mode == 'wrong' and puzzle.status != solved_status:
+                #             print 'WRONG ' + puzzle.title + ' = ' + rest
+                #             PuzzleWrongAnswer.objects.get_or_create(puzzle=puzzle, answer=rest)
+
+        print("Finished answerscrape run")
