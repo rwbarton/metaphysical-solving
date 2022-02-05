c03ded0c26cd8f162644e7511889f4d55e5be83e
Andrew <aethelred@gmail.com>
2022-02-05 18:02:56 +0000

    Soldier on in the face of some issues with Zulip being misconfigured

--- puzzles/zulip.py
+++ puzzles/zulip.py
@@ -11,13 +11,16 @@ def zulip_send(user, stream, subject, message):
         return
 
     print (user, stream, subject, message)
-    # Need to wait for this one to finish so that we know the stream exists (blargh).
-    subprocess.check_call([os.path.join(settings.PROJECT_ROOT,"zulip/api/examples/subscribe"),
-                     "--config-file", "/etc/puzzle/zulip/b+logger.conf",
-                     "--streams", stream])
-    subprocess.Popen([os.path.join(settings.PROJECT_ROOT,"zulip/api/bin/zulip-send"),
-                      "--config-file", "/etc/puzzle/zulip/%s.conf" % (user,),
-                      "--stream", stream, "--subject", subject, "--message", message.encode('utf-8')])
+    try:
+        # Need to wait for this one to finish so that we know the stream exists (blargh).
+        subprocess.check_call([os.path.join(settings.PROJECT_ROOT,"zulip/api/examples/subscribe"),
+                               "--config-file", "/etc/puzzle/zulip/b+logger.conf",
+                               "--streams", stream])
+        subprocess.Popen([os.path.join(settings.PROJECT_ROOT,"zulip/api/bin/zulip-send"),
+                          "--config-file", "/etc/puzzle/zulip/%s.conf" % (user,),
+                          "--stream", stream, "--subject", subject, "--message", message.encode('utf-8')])
+    except FileNotFoundError:
+        sys.stderr.write('Failed find Zulip API\n')
 
 try:
     zulip_create_settings = json.load(open('/etc/puzzle/zulip/create.json'))
@@ -28,19 +31,27 @@ def zulip_create_user(email, full_name, short_name):
     if zulip_create_settings is None:
         return
 
-    response = subprocess.check_output(
-        ['/usr/bin/curl',
-         '--insecure',
-         '--silent',
-         '-X', 'POST',
-         '-u', '%s:%s' % (zulip_create_settings['email'], zulip_create_settings['api_key']),
-         settings.ZULIP_SERVER_URL + '/api/v1/users',
-         '-d', 'email=' + email,
-         '-d', 'password=' + binascii.b2a_hex(os.urandom(15)).decode(encoding='ASCII'),
-         '-d', b'full_name=' + full_name.encode('utf-8'),
-         '-d', b'short_name=' + short_name.encode('utf-8')])
-
-    success = (json.loads(response.decode('utf-8')))['result'] == 'success'
+    try:
+        response = subprocess.check_output(
+            ['/usr/bin/curl',
+             '--insecure',
+             '--silent',
+             '-X', 'POST',
+             '-u', '%s:%s' % (zulip_create_settings['email'], zulip_create_settings['api_key']),
+             settings.ZULIP_SERVER_URL + '/api/v1/users',
+             '-d', 'email=' + email,
+             '-d', 'password=' + binascii.b2a_hex(os.urandom(15)).decode(encoding='ASCII'),
+             '-d', b'full_name=' + full_name.encode('utf-8'),
+             '-d', b'short_name=' + short_name.encode('utf-8')],
+            timeout=5)
+        success = (json.loads(response.decode('utf-8')))['result'] == 'success'
+    except subprocess.TimeoutExpired:
+        response = 'Timed out!\n'
+        success = False
+    except subprocess.CalledProcessError as err:
+        response = '%s returned exit code %d\n'%(err.cmd,err.returncode)
+        success = False
+        
     if not success:
         sys.stderr.write('Failed to create user (%s, %s, %s): %s' %
                          (email, full_name, short_name, response))
