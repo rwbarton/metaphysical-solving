8e8b76c26508d6144edc4b479a626ce0af2795f9
Andrew <aethelred@gmail.com>
2023-12-17 22:03:03 +0000

    instead of making spreadsheets world-readable, move them to a designated (shared) folder

--- puzzles/googlespreadsheet.py
+++ puzzles/googlespreadsheet.py
@@ -2,6 +2,7 @@ import json
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
 from googleapiclient import discovery
+from django.conf import settings
 
 _google_config = None
 
@@ -21,18 +22,27 @@ def create_google_spreadsheet(title):
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
     service = discovery.build('sheets', 'v4', http=http,
                               discoveryServiceUrl=discoveryUrl)
-
+    driveService = discovery.build('drive','v3',credentials=credentials)
     new_spreadsheet = service.spreadsheets().create(
         body={"properties": {"title": title}}).execute()
+    if (settings.FOLDER_ID):
+        file_id = new_spreadsheet['spreadsheetId']
+        file = driveService.files().get(fileId=file_id, fields="parents").execute()
+        previous_parents = ",".join(file.get("parents"))    
+        driveService.files().update(
+            fileId=file_id,
+            addParents=settings.FOLDER_ID,
+            removeParents=previous_parents,
+            fields="id, parents",
+        ).execute()
+    else:
+        acl_entry = {
+            'type': 'anyone',
+            'role': 'writer',
+            'allowFileDiscovery': False
+        }
+        service = discovery.build('drive', 'v3', http=http)
+        service.permissions().create(fileId=new_spreadsheet['spreadsheetId'],
+                                body=acl_entry).execute()
 
-
-    acl_entry = {
-        'type': 'anyone',
-        'role': 'writer',
-        'allowFileDiscovery': False
-    }
-
-    service = discovery.build('drive', 'v3', http=http)
-    service.permissions().create(fileId=new_spreadsheet['spreadsheetId'],
-                                 body=acl_entry).execute()
     return new_spreadsheet['spreadsheetUrl']
