91f20db067a0c84d59c0e3843099e32112978d7f
Allen Rabinovich <allen.ra@gmail.com>
2025-01-05 04:13:49 +0000

    Add a dynamically updating MOTD with notifications

--- solving/urls.py
+++ solving/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
 
     re_path(r'^overview/$', views.overview, name='puzzles.views.overview'),
     re_path(r'^api_overview', views.api_overview, name="puzzles.views.api_overview"),
+    re_path(r'^api_motd', views.api_motd, name="puzzles.views.api_motd"),
     re_path(r'^profile_photo', views.profile_photo, name='puzzles.views.profile_photo'),
     re_path(r'^overview/(\d+)/$',  views.overview_by, name='puzzles.views.overview_by'),
     re_path(r'^whowhat/$', views.who_what, name='puzzles.views.who_what'),
-
     re_path(r'^puzzle/(\d+)/$',  views.puzzle, name='puzzles.views.puzzle'),
     #re_path(r'^puzzle_new/(\d+)/$',  views.puzzle_new, name='puzzles.views.puzzle_new'),
     re_path(r'^puzzle_bottom/(\d+)/$',  views.puzzle_bottom, name='puzzles.views.puzzle_bottom'),
